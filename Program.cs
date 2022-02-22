7e13a6e73064c0738137a4be691ee4cb4cfb9d07
AJ Vossebelt <bertilvossebelt@gmail.com>
2022-02-22 15:49:44 +0100

    Enabled CORS for localhost:3000

--- Program.cs
+++ Program.cs
@@ -2,13 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using vogels_api.Data;
 using vogels_api.Services;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var serverVersion = new MariaDbServerVersion(new Version(10, 6, 4));
 
 // Add services to the container.
-builder.Services.AddCors();
-builder.Services.AddControllers();
 
 // Database connection with Pomelo Entity Framework
 builder.Services.AddDbContext<UserContext>(
@@ -27,6 +26,17 @@ builder.Services.AddScoped<JwtService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+    options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        }
+    ));
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -38,15 +48,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.UseCors(options => options
-    .WithOrigins(new []{"http://localhost:3000"})
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .AllowCredentials()
-);
 
 app.Run();
